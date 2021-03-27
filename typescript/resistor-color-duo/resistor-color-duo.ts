62c4204ae182c98f155275ba5e3ab54ae2b38860
Michael McGuiness <michaelmcguiness123@gmail.com>
2021-03-27 14:35:17 -0600

    solution optimization

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,27 +1,13 @@
-type Color =  'black' | 'brown' | 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'violet' | 'grey' | 'white'
+const predefinedColors =  ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
 
 export class ResistorColor {
-  private colors: Color[]
+  private colors: string[]
 
-  constructor(colors: Color[]) {
+  constructor(colors: string[]) {
     if (colors.length < 2) {
       throw new Error('At least two colors need to be present')
     }
     this.colors = colors
   }
-  value = (): number => {
-    const map = {
-      "black": 0,
-      "brown": 1,
-      "red": 2,
-      "orange": 3,
-      "yellow": 4,
-      "green": 5,
-      "blue": 6,
-      "violet": 7,
-      "grey": 8,
-      "white": 9
-    }
-    return map[this.colors[0]] * 10 + map[this.colors[1]]
-  }
+  value = (): number => predefinedColors.indexOf(this.colors[0]) * 10 + predefinedColors.indexOf(this.colors[1])
 }
