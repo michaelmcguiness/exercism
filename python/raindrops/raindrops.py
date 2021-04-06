c0a2158dfeaec898e510285905fcd823b359b6fd
Michael McGuiness <michaelmcguiness123@gmail.com>
2021-04-06 09:22:05 -0400

    use list comprehension

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,11 +1,6 @@
 SOUND_MAP = {3: "Pling", 5: "Plang", 7: "Plong"}
 
 
-def convert(number: int):
-    r = []
-    for sound in SOUND_MAP:
-        if number % sound == 0:
-            r.append(SOUND_MAP[sound])
-    if not r:
-        r.append(str(number))
-    return "".join(r)
+def convert(number: int) -> str:
+    r = "".join([SOUND_MAP[sound] for sound in SOUND_MAP if number % sound == 0])
+    return r if r else str(number)
